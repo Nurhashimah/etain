dc7334844dbd536afc80a4c6f50c204d440cec6e
Nurhashimah <nurhashimah77@gmail.com>
2016-05-10 15:10:24 +0800

    rev

--- config/deploy.rb
+++ config/deploy.rb
@@ -67,6 +67,7 @@ namespace :deploy do
   task :symlink_config_files do
     on roles(:all) do |host|
       execute "#{ sudo } ln -s #{ deploy_to }/shared/config/database.yml #{ current_path }/config/database.yml"
+    end
   end
 end
 
