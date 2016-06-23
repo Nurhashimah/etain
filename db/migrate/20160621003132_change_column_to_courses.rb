bffcf3dc2807bd541899179b06f683615798560f
Yaacob Noor <maxcobmara@gmail.com>
2016-06-23 12:23:05 +0800

    lecturer details

--- db/migrate/20160621003132_change_column_to_courses.rb
+++ db/migrate/20160621003132_change_column_to_courses.rb
@@ -1,8 +1,8 @@
 class ChangeColumnToCourses < ActiveRecord::Migration
   def change
-    remove_column :courses, :duration, :integer
-    add_column :courses, :duration, :decimal
-    remove_column :courses, :duration_type, :string
-    add_column :courses, :duration_type, :integer
+    remove_column   :courses, :duration, :integer
+    add_column      :courses, :duration, :decimal
+    remove_column   :courses, :duration_type, :string
+    add_column      :courses, :duration_type, :integer
   end
 end
---
+++ db/migrate/20160622000001_revert_columns_to_lecturers.rb
@@ -0,0 +1,9 @@
+class AddColumnsToLecturers < ActiveRecord::Migration
+  def change
+    rename_column :lecturers, :mykadno,     :details
+    remove_column    :lecturers, :position,    :string
+    remove_column    :lecturers, :address,     :string
+    remove_column    :lecturers, :qualifications, :text
+    remove_column    :lecturers, :experiences, :text
+  end
+end
